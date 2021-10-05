c03b2aef8842e344c9d64de318fff468155b4e23
Stefano Tavonatti <stefano.tavonatti@u-hopper.com>
2021-10-05 11:53:24 +0200

    Updated chaincode

--- chaincode-go/tsundoku_chaincode/chaincode/smartcontract.go
+++ chaincode-go/tsundoku_chaincode/chaincode/smartcontract.go
@@ -3,6 +3,7 @@ package chaincode
 import (
 	"encoding/json"
 	"fmt"
+	"time"
 
 	"github.com/hyperledger/fabric-contract-api-go/contractapi"
 )
@@ -12,16 +13,27 @@ type SmartContract struct {
 	contractapi.Contract
 }
 
-type Asset struct {
-	ID    string `json:"ID"`
-	Color string `json:"color"`
+// Consent to be stored in the blockchain.
+// ID is the hash of the user email
+// UserConsent is boolean that specify if the user has given its consent
+// LastUpdate is contain the epoch time (in seconds) of the last update of the asset
+type Consent struct {
+	ID                string `json:"ID"`
+	UserConsent       bool   `json:"consent"`
+	PrivacyPolicyHash string `json:"privacyPolicyHash"`
+	LastUpdate        int64  `json:"lastUpdate"`
 }
 
-func (s *SmartContract) CreateAsset(ctx contractapi.TransactionContextInterface, id string, color string) error {
+//UpdateConsent update the user consent
+func (s *SmartContract) UpdateConsent(ctx contractapi.TransactionContextInterface, id string, consent bool, privacyPolicyHash string) error {
 
-	asset := Asset{
-		ID:    id,
-		Color: color,
+	now := time.Now()
+
+	asset := Consent{
+		ID:                id,
+		UserConsent:       consent,
+		PrivacyPolicyHash: privacyPolicyHash,
+		LastUpdate:        now.Unix(),
 	}
 	assetJSON, err := json.Marshal(asset)
 	if err != nil {
@@ -31,8 +43,8 @@ func (s *SmartContract) CreateAsset(ctx contractapi.TransactionContextInterface,
 	return ctx.GetStub().PutState(id, assetJSON)
 }
 
-// ReadAsset returns the asset stored in the world state with given id.
-func (s *SmartContract) ReadAsset(ctx contractapi.TransactionContextInterface, id string) (*Asset, error) {
+// ReadConsent read the user consent
+func (s *SmartContract) ReadConsent(ctx contractapi.TransactionContextInterface, id string) (*Consent, error) {
 	assetJSON, err := ctx.GetStub().GetState(id)
 	if err != nil {
 		return nil, fmt.Errorf("failed to read from world state: %v", err)
@@ -41,7 +53,7 @@ func (s *SmartContract) ReadAsset(ctx contractapi.TransactionContextInterface, i
 		return nil, fmt.Errorf("the asset %s does not exist", id)
 	}
 
-	var asset Asset
+	var asset Consent
 	err = json.Unmarshal(assetJSON, &asset)
 	if err != nil {
 		return nil, err
