66c1db013497a453a6c72e26b390f81993200819
Stefano Tavonatti <stefano.tavonatti@u-hopper.com>
2021-10-11 13:41:46 +0200

    Updated network name

--- run.sh
+++ run.sh
@@ -7,5 +7,5 @@ then
   export $(cat ./docker-compose/.env | sed 's/#.*//g' | xargs)
 fi
 
-docker network create tsundoku-${ENV}
+docker network create tsundoku-blockchain-${ENV}
 docker-compose -f docker-compose/docker-compose-solo.yaml --project-name=tsundoku-blockchain-${ENV} up -d
